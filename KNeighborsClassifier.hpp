b0dc08f90d1b877326062c8d40e2c50bde4501cb
Soarex16 <soarex16@gmail.com>
2019-03-17 22:49:53 +0300

    Beginning of hard refactoring of the entire algorithm

--- KNeighborsClassifier.hpp
+++ KNeighborsClassifier.hpp
@@ -14,27 +14,127 @@
 #include <functional>
 
 namespace knn {
+    /**
+     * To improve the readability of the code, a number of types are declared for:
+     * feature vector
+     * test data labels
+     * metric function
+     */
+    typedef const std::valarray<double>& featureVec;
+    typedef double label;
+    typedef const std::function<double(featureVec, featureVec, const std::valarray<double>&)>& metricFunc;
+
+    /**
+     * Uniform distribution of neighbors weights
+     * @param distances vector of distances from one point to their neighbors
+     * @return vector of weights
+     */
     std::valarray<double> uniformWeights(const std::valarray<double> &distances) {
         std::valarray<double> equalWeights(distances.size(), 1.0 / distances.size());
 
         return equalWeights;
     }
 
+    /**
+     * Inverse square (1/d^2) distribution of neighbors weights
+     * @param distances vector of distances from one point to their neighbors
+     * @return vector of weights
+     */
     std::valarray<double> distanceWeights(const std::valarray<double> &distances) {
         std::valarray<double> inverseDist = distances.apply([](double dist) -> double {return std::pow(dist, -2);});
 
         return inverseDist;
     }
 
-    double euclideanDistance(const std::valarray<double>& x, const std::valarray<double>& y, const std::valarray<double>& metricParams) {
+    /**
+     * Standard euclidean metric (square root of the sum of squared differences)
+     * @param x fisrt point
+     * @param y second point
+     * @param metricParams additional metric parameters (i.e. p for minkowsi metric)
+     * @return distance between points in current metric
+     */
+    double euclideanDistance(featureVec x, featureVec y, const std::valarray<double>& metricParams) {
         return std::sqrt(std::pow(x - y, 2).sum());
     }
 
-    double minkowskiDistance(const std::valarray<double>& x, const std::valarray<double>& y, const std::valarray<double>& metricParams) {
+    /**
+     * Minkowski metric sum[(x - y)^p]^(1/p)
+     * @param x fisrt point
+     * @param y second point
+     * @param metricParams vector, the first component of which is the exponent (p)
+     * @return distance between points in current metric
+     */
+    double minkowskiDistance(featureVec x, featureVec y, const std::valarray<double>& metricParams) {
         return std::pow(std::pow(std::abs(x - y), metricParams[0]).sum(), 1 / metricParams[0]);
     }
 
-    void brute(const std::vector<std::valarray<double>>& x, std::vector<std::vector<double>>& distances, int k) {
+    /**
+     * Base class for all storages of points.
+     * Point storage must provide function for calculating distances.
+     * The main goal of this class is to move data to a separate storage, which allows adding,
+     * deleting points, finding the distance between them, and also searching for K nearest neighbors
+     */
+    class PointsStorage {
+     protected:
+      metricFunc metric;
+      const std::valarray<double>& metricParams;
+
+     public:
+      PointsStorage(metricFunc m, const std::valarray<double>& params) : metric(m), metricParams(params) {}
+
+      /**
+       * Note: need to do something about it to increase the readability of the code
+       * @param queryPoints vector of points for which neighbors are searched
+       * @param k number of neighbors of each point
+       * @return A vector of pairs, where the first component of the pair contains
+       * the distances to points, and the second component stores their IDs
+       */
+      virtual std::vector<std::pair< std::valarray<double>, std::valarray<int> >> getNeighbors(const std::vector<featureVec>& queryPoints, int k) = 0;
+
+      // The following method are needed to establish a bijection between training data, their labels and identifiers.
+      virtual label getLabel(int idx) = 0;
+
+      /**
+       * Operator that allows to get the index of a point by the vector, which represents that point
+       * @param feature vector, that represents point coordinates in N dimensional space
+       * @return index inside structure
+       */
+      virtual int operator[](featureVec point) = 0;
+
+      /**
+       * Operator that allows to get the feature vector of a point by its internal index
+       * @param idx index of a point inside storage
+       * @return feature vector of a point with given index
+       */
+      virtual featureVec operator[] (int idx) = 0;
+
+      /**
+       * Methods that implement the addition and removal of a set of points from the storage.
+       * These methods are necessary because some storage implementations (for example, a KD tree)
+       * can work more efficiently (as I think). Also, these methods allow you to implement additional fitting.
+       * @param points point vector to add or remove
+       */
+      virtual void addPoints(std::vector<featureVec> points) = 0;
+      virtual void deletePoints(std::vector<featureVec> points) = 0;
+
+      /**
+       * Calculates distance between two points
+       * @param p1 first point
+       * @param p2 second point
+       * @return distance between points
+       */
+      double distanceBetween(featureVec p1, featureVec p2) {
+          return metric(p1, p2, metricParams);
+      }
+
+      virtual ~PointsStorage() = default;
+    };
+
+
+
+
+
+    void brute(const std::vector<featureVec>& x, int k) {
         for (size_t i = 0; i < x.size(); ++i) {
             std::vector<double> distancesToCurrent(x.size() + 1);
             distancesToCurrent[x.size()] = 0;
